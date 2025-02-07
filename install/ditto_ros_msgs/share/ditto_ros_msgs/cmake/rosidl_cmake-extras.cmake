1bd7f2270483defb5b8b986177733ec810878849
johnnykindermann <kidsmanjohn@gmail.com>
2025-02-07 22:53:25 +0800

    add features and attributes ros topics

--- install/ditto_ros_msgs/share/ditto_ros_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ditto_ros_msgs_IDL_FILES "msg/AssetMetadata.idl;msg/Alert.idl;msg/Relationship.idl;msg/MachineStatus.idl")
-set(ditto_ros_msgs_INTERFACE_FILES "msg/AssetMetadata.msg;msg/Alert.msg;msg/Relationship.msg;msg/MachineStatus.msg")
